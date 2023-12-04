bf40d7df6d6664f70ea6c9c508212fa6b5e806a0
Armas <armasnsoft@gmail.com>
2023-12-04 19:13:38 +0000

    FEAT added a folder Scripts and a Test script

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+    void Start() {
+        Debug.Log("This is a test script");
+    }
+}
